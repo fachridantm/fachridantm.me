1e574126a9ba2743e0c80e88657f47ce47b6c750
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2023-11-13 23:46:39 +0700

    fix: 404 not found handling

--- app/root.tsx
+++ app/root.tsx
@@ -45,3 +45,28 @@ export default function App() {
     </html>
   );
 }
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>
+          {
+            isRouteErrorResponse(error)
+              ? `${error.status} ${error.statusText}`
+              : error instanceof Error
+                ? error.message
+                : "Unknown Error"
+          }
+        </h1>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
