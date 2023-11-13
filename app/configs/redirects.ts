8054860862e56491ce6cc94005f3dd9df1e2998e
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2023-11-14 01:40:11 +0700

    fix: resume & portfolio redirect link

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -1,7 +1,7 @@
 export const configRedirects = [
     { path: "/", url: "https://fachridantm.github.io" },
-    { path: "/resume", url: "/pdf/Fachridan%20Tio%20Mu'afa_CV.pdf" },
-    { path: "/portfolio", url: "/pdf/Fachridan%20Tio%20Mu'afa_Portfolio.pdf" },
+    { path: "/resume", url: "https://fachridantm.github.io/assets/pdf/Fachridan Tio Mu'afa_CV.pdf" },
+    { path: "/portfolio", url: "https://fachridantm.github.io/assets/pdf/Fachridan%20Tio%20Mu'afa_Portfolio.pdf" },
     { path: "/deck", url: "https://speakerdeck.com/fachridantm" },
     { path: "/speakership", url: "https://drive.google.com/drive/folders/1UEq_uKNvUO9zBm4s-eWJHzgGjz0mx-68?usp=sharing" },
     { path: "/mentorship", url: "https://calendar.app.google/AvPhX5gM7ZeZvdFj9" },
