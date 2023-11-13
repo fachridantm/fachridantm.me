dfae35ffda598ede6f5754a13a149792a0085361
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2023-11-14 00:51:42 +0700

    fix: route bangkit

--- app/routes/projects.bangkit.tsx
+++ app/routes/projects.bangkit.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@vercel/remix";
 
-export const config = { runtime: "edge" };
+export const config = { runtime: "bangkit" };
 
 export const meta: MetaFunction = () => [{ title: "Bangkit Merch Store" }];
 
