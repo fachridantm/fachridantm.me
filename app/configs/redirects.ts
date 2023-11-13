941e0b5f5e59d4990a8046507d388fdf4ca1dc0f
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2023-11-13 14:09:07 +0700

    remove / path

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -1,5 +1,4 @@
 export const configRedirects = [
-    { path: "/", url: "https://fachridantm.github.io/" },
     { path: "/resume", url: "https://fachridantm.github.io/assets/pdf/Fachridan%20Tio%20Mu'afa_CV.pdf" },
     { path: "/portfolio", url: "https://fachridantm.github.io/assets/pdf/Fachridan%20Tio%20Mu'afa_Portfolio.pdf" },
     { path: "/deck", url: "https://speakerdeck.com/fachridantm" },
