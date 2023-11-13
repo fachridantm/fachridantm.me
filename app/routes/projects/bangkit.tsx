88dbce505c9c574c20a2dcfa96a9153e53bdbaf5
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2023-11-13 23:19:41 +0700

    fix: bangkit route and 404 not found handling

--- app/routes/projects/bangkit.tsx
+++ app/routes/projects/bangkit.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@vercel/remix";
 
-export const config = { runtime: "/projects/bangkit" };
+export const config = { runtime: "edge" };
 
 export const meta: MetaFunction = () => [{ title: "Bangkit Merch Store" }];
 
