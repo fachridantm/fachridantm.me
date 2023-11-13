9562cfdc21810953404a0987bdce67b6f115903a
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2023-11-14 01:24:41 +0700

    rollback

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -1,6 +1,7 @@
 export const configRedirects = [
-    { path: "/resume", url: "https://fachridantm.me/public/assets/pdf/Fachridan%20Tio%20Mu'afa_CV.pdf" },
-    { path: "/portfolio", url: "https://fachridantm.me/public/assets/pdf/Fachridan%20Tio%20Mu'afa_Portfolio.pdf" },
+    { path: "/", url: "https://fachridantm.github.io" },
+    { path: "/resume", url: "/pdf/Fachridan%20Tio%20Mu'afa_CV.pdf" },
+    { path: "/portfolio", url: "/pdf/Fachridan%20Tio%20Mu'afa_Portfolio.pdf" },
     { path: "/deck", url: "https://speakerdeck.com/fachridantm" },
     { path: "/speakership", url: "https://drive.google.com/drive/folders/1UEq_uKNvUO9zBm4s-eWJHzgGjz0mx-68?usp=sharing" },
     { path: "/mentorship", url: "https://calendar.app.google/AvPhX5gM7ZeZvdFj9" },
