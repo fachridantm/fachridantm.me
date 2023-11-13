772e4485e1480dd4f98deaa75a39c0a1ae72e816
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2023-11-13 14:45:25 +0700

    refactor: merge from fachridantm.github.io

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -1,6 +1,6 @@
 export const configRedirects = [
-    { path: "/resume", url: "https://fachridantm.github.io/assets/pdf/Fachridan%20Tio%20Mu'afa_CV.pdf" },
-    { path: "/portfolio", url: "https://fachridantm.github.io/assets/pdf/Fachridan%20Tio%20Mu'afa_Portfolio.pdf" },
+    { path: "/resume", url: "https://fachridantm.me/public/assets/pdf/Fachridan%20Tio%20Mu'afa_CV.pdf" },
+    { path: "/portfolio", url: "https://fachridantm.me/public/assets/pdf/Fachridan%20Tio%20Mu'afa_Portfolio.pdf" },
     { path: "/deck", url: "https://speakerdeck.com/fachridantm" },
     { path: "/speakership", url: "https://drive.google.com/drive/folders/1UEq_uKNvUO9zBm4s-eWJHzgGjz0mx-68?usp=sharing" },
     { path: "/mentorship", url: "https://calendar.app.google/AvPhX5gM7ZeZvdFj9" },
